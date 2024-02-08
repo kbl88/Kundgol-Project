c83cff031194aed28ac3490ad0146769f7e6acbb
Lathaneha <neha88job@gmail.com>
2024-02-08 11:02:44 +0530

    Added CorsConfiguration to SecurityService class and removed from main class.

--- kundgol-service/src/main/java/com/kbl/kundgolservice/KundgolServiceApplication.java
+++ kundgol-service/src/main/java/com/kbl/kundgolservice/KundgolServiceApplication.java
@@ -2,27 +2,12 @@ package com.kbl.kundgolservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.kbl.kundgolservice.dto"})
-public class KundgolServiceApplication {
 
+public class KundgolServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KundgolServiceApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
 	}
 
 }
--- kundgol-service/src/main/java/com/kbl/kundgolservice/config/SecurityConfig.java
+++ kundgol-service/src/main/java/com/kbl/kundgolservice/config/SecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -37,7 +42,7 @@ public class SecurityConfig {
     // Configuring HttpSecurity
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors().and().csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/auth/generateToken")
                         .permitAll())
                 .authorizeHttpRequests(request -> request.requestMatchers("/user/**").authenticated())
@@ -47,6 +52,15 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     // Password Encoding
